0e5dc6d55ee68d376f8dc7f990cc925ff677ad8c
Salim-54 <ndayisabyesalim54@gmail.com>
2022-12-21 12:01:20 +0200

    ft: added routes

--- delivering.py
+++ delivering.py
@@ -2,8 +2,43 @@ import flet as ft
 from flet import *
 
 
+def re_usable_static(key, value):
+    return Row(controls=[
+        Container(
+            margin=margin.only(left=30),
+            width=150,
+            content=Row(controls=[
+                Text(key, color="black", expand=True),
+                Text(" :   ", color="black")
+            ],
+                alignment=MainAxisAlignment.SPACE_BETWEEN,
+                vertical_alignment=CrossAxisAlignment.CENTER,
+            ),),
+
+        Text(value, color="black", expand=True),],
+        alignment=MainAxisAlignment.SPACE_AROUND,
+        vertical_alignment=CrossAxisAlignment.CENTER
+    )
+
+
+def re_usable(number, med, dose, time):
+    return Row(controls=[
+        Row(controls=[
+            Text(number, color="black"),
+            Text(med, color="black"),
+        ],
+            alignment=MainAxisAlignment.CENTER,
+            vertical_alignment=CrossAxisAlignment.CENTER),
+        Text(f"{dose}", color="black"),
+        Text(time, color="black"),],
+        alignment=MainAxisAlignment.SPACE_AROUND,
+        vertical_alignment=CrossAxisAlignment.CENTER
+    )
+
+
 def delivering():
     _delivering = Column(
+        visible=True if screen == 2 else False,
         controls=[
             Text("DELIVERING", size=30, color="black"),
             Container(
